1b0678c3a85cd4e549cfdbe8a290c88cb238e65b
aayushaaaa <aayushaaaa.i@gmail.com>
2024-06-19 10:49:11 +0545

    class member variables and functions

--- graph.h
+++ graph.h
@@ -1,1 +1,23 @@
-#include<iostream>
+#include <iostream>
+class Graph
+{   
+    private:
+        int numOfVertices;
+        bool directed;
+    public:
+        Graph(int numOfVertices, bool directed);
+        bool isEmpty();
+        bool isDirected();
+        void addVertex(int newVertex);
+        void addEdge(int vertex1,int vertex2);
+        void removeVertex(int vertexToRemove);
+        void removeEdge(int vertex1,int vertex2);
+        int numVertices();
+        int numEdges();
+        int indegree(int vertex);
+        int outdegree(int vertex);
+        int degree(int vertex);
+        int neighbours(int vertex);
+        bool neighbour(int vertex1,int vertex2);
+
+};
